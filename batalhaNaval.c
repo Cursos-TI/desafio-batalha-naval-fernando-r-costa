b01cf8baebfeecd655afeb695173e47a79ef8da4
Fernando R Costa <fernando.r.costa@outlook.com>
2025-06-05 10:23:40 +0000

    Posicionamento dos navios e entrega do nivel novato do jogo Batalha Naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -12,6 +12,46 @@ int main() {
         }
     }
 
+    // Define o navio horizontal
+    int navio_horizontal[3] = {3, 3, 3};
+    int linha_navio_horizontal = 2;
+    int coluna_inicio_horizontal = 2;
+
+    // Se couber, imprime o navio horizontal
+    if (coluna_inicio_horizontal + 3 <= colunas_tabuleiro) {
+        for (int i = 0; i < 3; i++) {
+            tabuleiro[linha_navio_horizontal][coluna_inicio_horizontal + i] = navio_horizontal[i];
+        }
+    } else {
+        printf("Erro: o navio está posicionado fora do tabuleiro");
+    }
+
+    // Define o navio vertical
+    int navio_vertical[3] = {3, 3, 3};
+    int linha_inicio_vertical = 5;
+    int coluna_navio_vertical = 4;
+
+    // Verifica se cabe e não sobrepõe para imprimir o navio vertical
+    if (linha_inicio_vertical + 3 <= linhas_tabuleiro) {
+        int pode_colocar = 1;
+        for (int i = 0; i < 3; i++) {
+            if (tabuleiro[linha_inicio_vertical + i][coluna_navio_vertical] != 0) {
+                pode_colocar = 0;
+                break;
+            }
+        }
+
+        if (pode_colocar) {
+            for (int i = 0; i < 3; i++) {
+                tabuleiro[linha_inicio_vertical + i][coluna_navio_vertical] = navio_vertical[i];
+            }
+        } else {
+            printf("Erro: sobreposição detectada.\n");
+        }
+    } else {
+        printf("Erro: o navio não cabe na posição especificada.\n");
+    }
+
     // Imprime o cabeçalho com letras
     printf("   ");
     for (int j = 0; j < colunas_tabuleiro; j++) {
